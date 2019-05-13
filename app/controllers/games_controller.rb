39624b6a40c3470a20f8e18cdb7685c6512794ca
moral nicolas <mrwallp@gmail.com>
2019-05-13 17:39:13 +0200

    gameletter

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,12 @@
 class GamesController < ApplicationController
   def new
-    @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }.join(' ').upcase
+    alphabet = ('a'..'z').to_a
+    vowell = %w(a e i o u y)
+    @letters = []
+    8.times { @letters << alphabet.sample }
+    2.times { @letters << vowell.sample }
   end
 
   def score
-    raise
   end
 end
